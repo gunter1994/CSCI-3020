0a7fd2b602a8aeb6b38c1f35f84b4796c2f04fca
Nathaniel Yearwood <nathaniel.yearwood@uoit.net>
2017-03-13 00:10:26 -0400

    Fixed checkCol and checkRow, started checkBoxes

--- Lab3/lab3.c
+++ Lab3/lab3.c
@@ -89,14 +89,15 @@ int main(int argc, char *argv[])
 
 // check if rows contain numbers 1-9
 void *checkRows(void *thread) {
-    _Bool numTest[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //defaults values to false
+    int numTest[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //defaults values to false
+    
     for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) { //checks for every number
-            numTest[puzzle[i][j] -1] = 1;
+        for(int j = 0; j < 9; j++) { //checks each row for every number
+            numTest[puzzle[i][j] -1] ++;
         }
         for(int k = 0; k < 9; k++) {
-            if(numTest[k] != 1) {  //if one num wasnt found, returns, and sets result
-                results[10] = 0;
+            if(numTest[k] > 1) {  //if one num occurs twice, sets result
+                results[thread] = 0;
                 printf("Rows failed"); //DELETE THIS
                 return thread;
             } else { //sets rest to false again
@@ -104,31 +105,51 @@ void *checkRows(void *thread) {
             }
         }
     }
-
+    results[thread] = 1;
+    return thread;
 }
 
 // check if columns contain numbers 1-9
 void *checkCols(void *thread) {
-    _Bool numTest[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //defaults values to false
+    int numTest[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //defaults values to false
+    
     for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) { //checks for every number
-            numTest[puzzle[j][i] -1] = 1;
+        for(int j = 0; j < 9; j++) { //checks each row for every number
+            numTest[puzzle[j][i] -1] ++;
         }
         for(int k = 0; k < 9; k++) {
-            if(numTest[k] != 1) {  //if one num wasnt found, returns, and sets result
-                results[10] = 0;
-                printf("cols failed"); //delete this
+            if(numTest[k] > 1) {  //if one num occurs twice, sets result
+                results[thread] = 0;
+                printf("Columns failed"); //DELETE THIS
                 return thread;
             } else { //sets rest to false again
                 numTest[k] = 0;
             }
         }
     }
+    results[thread] = 1;
     return thread;
 }
 
 // check if box contains numbers 1-9
-void *checkBoxes(void *params) {
+void *checkBoxes(void *params, int boxRow, int boxCol) {
+    int numTest[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //defaults values to false
     
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) { //checks each box for every number
+            numTest[puzzle[i*boxRow][j*boxCol] -1] ++;
+        }
+        for(int k = 0; k < 9; k++) {
+            if(numTest[k] > 1) {  //if one num occurs twice, sets result
+                results[thread] = 0;
+                printf("Boxes failed"); //DELETE THIS
+                return thread;
+            } else { //sets rest to false again
+                numTest[k] = 0;
+            }
+        }
+    }
+    results[thread] = 1;
+    return thread;
 }
 
