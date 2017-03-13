20f1174b845d26b5830f9ad5f997b38633d026bc
SNHouse <samantha.house@uoit.net>
2017-03-12 22:08:16 -0400

    set up threads, need to implement thread functions

--- Lab3/lab3.c
+++ Lab3/lab3.c
@@ -9,28 +9,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <string.h>
 
 //  macros and constants
 #define BUFFER_LEN 256
+#define NUM_THREADS 11
 
 // global environment variables
 int puzzle[9][9]; //stores the puzzle
-int results[11]; //stores results from each thread
+int results[NUM_THREADS]; //stores results from each thread
+
+// function declarations
+void *checkRows();
+void *checkCols();
+void *checkBoxes(void *params);
+
+struct thread_args {
+    int row;
+    int col;
+};
 
 int main(int argc, char *argv[])
 {   
     FILE *fp;
     char *line = NULL;
     char *token;
-    int row=-1,col=-1;
-    char buffer[BUFFER_LEN];
+    int i,row=-1,col=-1;
+    pthread_t threads[NUM_THREADS];
     size_t len = 0;
     ssize_t read;
 
     /* read in input file puzzle.txt */ 
     fp = fopen(argv[1], "r");
-
     while ((read = getline(&line, &len, fp)) != -1) 
     {
         row++;
@@ -41,8 +52,46 @@ int main(int argc, char *argv[])
             token = strtok(NULL," \n");
         }
     }
-    
     fclose(fp);
+
+    // set up threads to for checking boxes
+    struct thread_args args;
+      /*
+      [0][0], [0][3], [0][6],
+      [3][0], [3][3], [3][6],
+      [6][0], [6][3], [6][6]
+     */
+
+     args.row = 0;
+    for(i = 0; i < NUM_THREADS-2; i++) {
+        if(i%3 == 0) { args.row += 3; }
+        args.col = 3*(i % 3);
+        pthread_create(&threads[i], NULL, checkBoxes, &args);
+    }
+
+    // set up thread for checking rows
+    pthread_create(&threads[0], NULL, checkRows, NULL);
+
+    // set up thread for checking columns
+    pthread_create(&threads[1], NULL, checkCols, NULL);
+
+   
+    pthread_exit(NULL);
     return 0;
+}
+
+// check if row contains numbers 1-9
+void *checkRows() {
+
+}
+
+// check if column contains numbers 1-9
+void *checkCols() {
+
+}
+
+// check if box contains numbers 1-9
+void *checkBoxes(void *params) {
+    
 }
 
